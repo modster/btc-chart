c910dde57da4a3e19a3731b6122893371dbbd666
Mike G <unmodster@gmail.com>
2019-02-22 05:34:08 -0700

    tweaked dates

--- platforms/browser/www/index.js
+++ platforms/browser/www/index.js
@@ -1,5 +1,5 @@
 //API to fetch historical data of Bitcoin Price Index
-const api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2018-04-01';
+const api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2019-02-20';
 
 /**
  * Loading data from API when DOM Content has been loaded'.
--- www/sohaibnehal-cwmGZCw/README.md
+++
@@ -1,2 +0,0 @@
-# Create a line chart
-

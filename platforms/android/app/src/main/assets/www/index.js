a7d6cd9fc238460ca4db54dce15521fe45e327ca
Mike G <unmodster@gmail.com>
2019-02-24 10:19:42 -0700

    testing date function

--- platforms/android/app/src/main/assets/www/index.js
+++ platforms/android/app/src/main/assets/www/index.js
@@ -1,6 +1,76 @@
-//API to fetch historical data of Bitcoin Price Index
-const api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2019-02-20';
+/**********************************************************************************************************************************
+ *                                                                                                                                *
+ *                                                20th Century Fox News Presents:                                                 *
+ *                                                                                                                                *
+ *                                               Summer 2019's Blockbuster Hit App                                                *
+ *                                                                                                                                *
+ ***********************************************************************************************************************************                                                                                                                                *
+ *                                                                                                                                *
+ *                                             B  I  T  C  O  I  N  -  C  H  A  R  T                                              *
+ *                                                                                                                                *
+ *                                                             ~ * ~                                                              *
+ *                                                                                                                                *
+ *                                                     E p i s o d e  X I I I                                                     *
+ *                                                                                                                                *
+ *                                           ' B u t t C o i n  B r e a k s  W i n d '                                            *
+ *                                                                                                                                *
+ *                                                                                                                                *
+ **********************************************************************************************************************************
+ *                                                                                                                                *
+ *                                                                                                                                *
+ *                                                           Starring                                                             *
+ *                                                                                                                                *
+ *                                           M I K E  " T h e  B o s s "  B O S T O C K                                           *
+ *                                                                                                                                *
+ *                                                              and                                                               *
+ *                                                                                                                                *
+ *                                                            D 3 J S                                                             *
+ *                                                                                                                                *
+ *                                                                                                                                *
+ **********************************************************************************************************************************
+ *                                                                                                                                *
+ *                                                      Executive Producer:                                                       *
+ *                                                                                                                                *
+ *                                                     M I K E   G R E E F F                                                      *
+ *                                                                                                                                *
+ *                                                       mike@greeffer.com                                                        *
+ *                                                                                                                                *
+ *                                                             ~ * ~                                                              *
+ *                                                                                                                                *
+ *                                                        Special Thanks:                                                         *
+ *                                                                                                                                *
+ *                                                       The Repo I cloned                                                        *
+ *                                                            My Wife                                                             *
+ *                                                         My Girlfriend                                                          *
+ *                                                      Maxwell House Coffee                                                      *
+ *                                                          CoinDesk API                                                          *
+ *                                                         Donald J Trump                                                         *
+ *                                                                                                                                *
+ *********************************************************************************************************************************/
 
+ /**
+ * Historical BPI endpoint:     https://api.coindesk.com/v1/bpi/historical/close.json (returns previous 31 days' price data)
+ * ?index=USD/CNY               The index to return data for. Defaults to USD.
+ * ?currency=<VALUE>            The currency to return the data in, specified in ISO 4217 format. Defaults to USD.
+ * ?start=<VALUE>&end=<VALUE>   Allows data to be returned for a specific date range. Must be listed as a pair of start and end
+ *                              parameters, with dates supplied in the YYYY-MM-DD format, e.g. 2013-09-01 for September 1st, 2013.
+ * ?for=yesterday               Specifying this will return a single value for the previous day. Overrides the start/end parameter.
+ * Sample Request:              https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05 
+ */
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+var yyyyMinusOne = yyyy-1;
+if(dd<10) {
+    dd = '0'+dd
+} 
+if(mm<10) {
+    mm = '0'+mm
+} 
+
+var api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start='+ yyyyMinusOne +'-'+ mm + '-'+ dd +'&end='+ yyyy +'-'+ mm +'-'+ dd;
+console.log(api)
 /**
  * Loading data from API when DOM Content has been loaded'.
  */
